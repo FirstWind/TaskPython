e4a7ffcf790f237a7d4bb0627942b836eaae2266
FirstWind <35399683+FirstWind@users.noreply.github.com>
2021-03-15 19:37:28 +0900

    Update TaskOtel.py

--- TaskOtel.py
+++ TaskOtel.py
@@ -1,94 +1,123 @@
 class Hotel:
-    """Родительский класс отеля"""
+    """Класс отеля"""
 
-    def __init__(self):
-        self.count_room = 10
-        self.rooms = {1: ['бизнес', 3000, '2-комнатная'], 2: ['бизнес', 5000, '2-комнатная'],
-                      3: ['люкс', 10000, '3-комнатная'], 4: ['эконом', 2500, '1-комнатная'],
-                      5: ['эконом', 1500, '1-комнатная'], 6: ['эконом', 2000, '1-комнатная'],
-                      7: ['эконом', 1500, '1-комнатная'], 8: ['эконом', 2000, '1-комнатная'],
-                      9: ['эконом', 1500, '1-комнатная'], 10: ['эконом', 2000, '1-комнатная']}
-        self.status = ['занята', 'свободна', 'на ремонте']
-        self.status_rooms = {1: [self.status[1], ''], 2: [self.status[1], ''], 3: [self.status[1], ''],
-                             4: [self.status[1], ''], 5: [self.status[1], ''], 6: [self.status[1], ''],
-                             7: [self.status[1], ''], 8: [self.status[1], ''], 9: [self.status[1], ''],
-                             10: [self.status[1], '']}
-
-
-class HotelRoom(Hotel):
-    """Класс комнат отеля"""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__()
-
-    def check_in_status_room(self, number_room):
-        return self.status_rooms.get(number_room)
-
-    def update_status_room(self, number_room, status):
-        state_check = self.check_in_status_room(number_room)
-        if state_check is not None:
-            self.status_rooms[number_room][0] = status
-            return True
-        else:
-            print(f'комната № {number_room} - Не существует')
-            return False
-
-    def update_human_in_room(self, name, number_room):
-        self.status_rooms[number_room][1] = name
-        self.view_rooms_free()
-        print(f'комната № {number_room} - {self.status_rooms[number_room][0]} {self.status_rooms[number_room][1]}')
-
-    def view_rooms(self):
-        for key, value in self.status_rooms.items():
-            print(f'комната № {key} - {" ".join(value)} {self.rooms[key][0]} {self.rooms[key][1]}')
-
-    def view_rooms_free(self):
-        for key, value in self.status_rooms.items():
-            if value[0] == self.status[1]:
-                print(f'комната № {key} - {" ".join(value)} {self.rooms[key][0]} {self.rooms[key][1]}')
+    def __init__(self, hotelname,  # название отеля
+                 adress,  # адрес отеля
+                 countsingleroom=3,  # количество однокомнатных номеров
+                 countdoublerooms=2,  # количество однокомнатных номеров
+                 counttriplerooms=1,  # количество однокомнатных номеров
+                 status=3,  # статус (количество звезд)
+                 ):
+        self.hotelname = hotelname
+        self.adress = adress
+        self.countsingleroom = countsingleroom
+        self.countdoublerooms = countdoublerooms
+        self.counttriplerooms = counttriplerooms
+        self.status = status
+        self.singlerooms = [Room(100 + i) for i in range(self.countsingleroom)]
+        self.doublerooms = [Room(200 + i, room_rate=2_000, room_area=2, classrooms='люкс') for i in range(self.countdoublerooms)]
+        self.triplerooms = [Room(300 + i, room_rate=3_000, room_area=3, classrooms='бизнесс') for i in range(self.counttriplerooms)]
+        self.allrooms = self.singlerooms + self.doublerooms + self.triplerooms
 
 
-class Visitor(HotelRoom):
-    """Класс посетителя"""
+    def spisok_free_number(self):
+        for i in self.allrooms:
+            if i.status == 'свободна':
+                print(f"{i.number} {i.classrooms}")
 
-    def __init__(self):
-        super().__init__()
+    def human_in_hotel(self, number_room, roomer):
+        for i in self.allrooms:
+            if i.number == number_room:
+                i.status = 'занята'
+                i.human = roomer
+                roomer.number_nomer = number_room
+                print(f'Добро пожаловать {i.human.roomername} в {self.hotelname}, ваш номер {number_room}')
 
-    def check_into(self, name, number_room):  # заселиться в оте8ль
-        if self.update_status_room(number_room, 'занята'):
-            self.update_human_in_room(name, number_room)
-        # print(f'{self.name} Ваш номер №{self.number_room} {self.rooms[self.number_room]}')
+    def human_out_hotel(self, roomer):
+        for i in self.allrooms:
+            if i.number == roomer.number_nomer:
+                i.status = 'свободна'
+                i.human = None
+                print(f'{roomer.roomername} выписали из номера {roomer.number_nomer} отеля {self.hotelname}')
 
-    def view_gost(self):
-        for key, value in self.status_rooms.items():
-            print(f'комната №{key} - {value} {self.rooms[key]}')
+    def human_replace_hotel(self, number_room_in, roomer):
+        self.human_out_hotel(roomer)
+        self.human_in_hotel(number_room_in, roomer)
+
+    def change_status_rooms(self, status, roomer):
+        for i in self.allrooms:
+            if i.number == roomer.number_nomer:
+                i.status = f'Ремонт! Причина: {status}'
+                print(f"{i.number} {i.classrooms} {i.status} проживает: {i.human.roomername}")
+
+    def spisokallrooms(self):
+        for i in self.allrooms:
+            if i.human != None:
+                print(f"{i.number} {i.classrooms} {i.status} {i.human.roomername}")
+            else:
+                print(f"{i.number} {i.classrooms} {i.status}")
 
 
-def main():
-    rooms = HotelRoom()
-    gost = Visitor()
+class Room:
+    """Класс комнат в отеле"""
+
+    def __init__(self, number=100,  # номер комнаты
+                 classrooms='эконом',  # класс номера (бизнесс, эконом, люкс...)
+                 room_area=1,  # количество комнат в номере
+                 room_rate=1_000,  # стоимость номера
+                 status='свободна',  # статус комнаты (свободна, ремонт, занята)
+                 ):
+        self.number = number
+        self.classrooms = classrooms
+        self.room_area = room_area
+        self.room_rate = room_rate
+        self.status = status
+        self.human = None
+
+class Roomer:
+    """Класс посетителей отеля"""
+
+    def __init__(self, roomername, age, document="паспорт"):
+        self.roomername = roomername
+        self.age = age
+        self.document = document
+        self.number_nomer = None
+
+if __name__ == '__main__':
+    otelastoria = Hotel('Hotel Astoria', 'г. Чита, ул. Бутина 120')
+
     while True:
         quest = '1 - Заселиться\n2 - Выселиться\n3 - Сменить номер\n4 - Отремонтировать\
-        \n5 - Выход\n'
+                \n5 - Просмотр всех комнат\
+                \n6 - Выход\n'
         try:
             str_vibor = input(quest)
         except ValueError:
             print("Ввели не число")
             continue
+
         if str_vibor == '1':
             name = input('Введите имя ')
-            rooms.view_rooms_free()
-            gost.check_into(name, int(input('Введите № номера для заселения ')))
+            age = input('Введите возраст ')
+            human = Roomer(name, age)
+            print(otelastoria.spisok_free_number())
+            number_room = int(input('Введите № номера для заселения '))
+            otelastoria.human_in_hotel(number_room, human)
+
         elif str_vibor == '2':
-            pass
+            otelastoria.human_out_hotel(human)
+
         elif str_vibor == '3':
-            pass
+            print(otelastoria.spisok_free_number())
+            number_room_in = int(input('Введите № номера для переселения '))
+            otelastoria.human_replace_hotel(number_room_in, human)
+
         elif str_vibor == '4':
-            pass
+            status = input('Введите название поломки ')
+            otelastoria.change_status_rooms(status, human)
+
         elif str_vibor == '5':
-            # gost.viewGost()
+            print(otelastoria.spisokallrooms())
+
+        elif str_vibor == '6':
             exit()
-
-
-if __name__ == '__main__':
-    main()
